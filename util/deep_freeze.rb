ecaa7effa98f23b227c973052c0ca0d1aff64d4f
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-01-15 15:48:29 +0200

    Add deep_freeze for Hash and Array, add documentation to YamlLoader, dup.freeze filename sources in YamlLoader

--- util/deep_freeze.rb
+++ util/deep_freeze.rb
@@ -1,8 +1,34 @@
 class Object
 
-	def deep_freeze
-		for var in instance_variables do instance_variable_get(var).freeze end
-		freeze
-	end
+  def deep_freeze
+    for var in instance_variables do instance_variable_get(var).freeze end
+    freeze
+  end
 
 end
+
+
+
+class Hash
+
+  def deep_freeze
+    for key, val in self do
+      key.deep_freeze
+      val.deep_freeze
+    end
+    freeze
+  end
+
+end
+
+
+
+class Array
+
+  def deep_freeze
+    each &:deep_freeze
+    freeze
+  end
+
+end
+
--- util/yaml_loader.rb
+++ util/yaml_loader.rb
@@ -4,8 +4,19 @@ require 'util/deep_dup'
 
 module Util
 
-# Loads various yaml databases and merges them
-# into a final result.
+# Preserves yaml sources and reloads them
+# when requested.
+#
+# Instead of loading each source upon
+# construction, each resource is preserved,
+# according to its type. Later, when a
+# {#reload} is requested, each source is
+# loaded anew.
+#
+# Finally, the results from all the sources
+# are merged into a final {Hash}. The
+# intermediate hashes are merged in the
+# order in which the sources have been provided.
 
 module YamlLoader
 
@@ -13,13 +24,31 @@ module YamlLoader
     super
   end
 
+  # Each argument can be:
+  # * A {Hash}: in this case the hash reference
+  #   is stored and the hash will be merged
+  #   as-is with the other results. This hash
+  #   is never modified by the {#YamlLoader},
+  #   and it is not copied or cached in any way:
+  #   any change in the hash instance will be
+  #   reflected at the next {#reload}.
+  # * A {String} filename ending in '.yaml':
+  #   in this case the filename is duplicated
+  #   and stored. Upon {#reload}, the file
+  #   indicated by this filename is read and
+  #   its contents parsed as YAML. The resulting
+  #   hash is merged with the results from
+  #   other sources.
+  # * Any other {String}: it is immediately
+  #   parsed as YAML and the resulting hash
+  #   is stored to be merged with other results.
   def initialize_yaml_loader *yamls
     @yamls = yamls.map do |yaml|
       case yaml
         when Hash then yaml
         when String then
           if File.extname(yaml) == '.yaml'
-            then yaml
+            then yaml.dup.freeze
             else YAML.load(yaml).freeze
           end
         else raise ArgumentError.new yaml.inspect
