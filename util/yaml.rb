4e1d71d6360543946cbccc0d5b0a55d505bfa28f
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-01-17 10:51:39 +0200

    load_yaml_file_if_it_exists returns empty hash for an empty YAML file

--- util/yaml.rb
+++ util/yaml.rb
@@ -7,7 +7,7 @@ module Yaml
 
   def load_yaml_file_if_it_exists path
     if File.exist? path
-      then YAML.load File.read path
+      then (YAML.load File.read path) || {}
       else {}
     end
   end
