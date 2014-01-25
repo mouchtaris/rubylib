5021a0c6a6ba904185f98d419f6a300d52f8760b
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-01-25 08:21:24 +0200

    Deep freeze translation tables

--- togr.rb
+++ togr.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+require 'util/deep_freeze'
 
 module Togr
   Simple = Hash[*%w{
@@ -9,31 +9,31 @@ module Togr
     A Α  B Β  G Γ  D Δ  E Ε  Z Ζ  H Η  U Θ  I Ι
     K Κ  L Λ  M Μ  N Ν  J Ξ  O Ο  P Π  R Ρ  S Σ
     T Τ  Y Υ  F Φ  X Χ  C Ψ  V Ω
-  }]
+  }].deep_freeze
 
   Stressed = Hash[*%w{
     a ά  e έ  h ή  i ί  o ό  y ύ  v ώ
-  }]
+  }].deep_freeze
 
-  Dialitics = Hash[*%w{ i ϊ  y ϋ }]
+  Dialitics = Hash[*%w{ i ϊ  y ϋ }].deep_freeze
 
-  StressedDialitics = Hash[*%w{ i ΐ  y ΰ }]
+  StressedDialitics = Hash[*%w{ i ΐ  y ΰ }].deep_freeze
 
-  CHAR_STRESS = ';'
-  CHAR_DIALIT = ':'
+  CHAR_STRESS = ';'.deep_freeze
+  CHAR_DIALIT = ':'.deep_freeze
   MOD_STRESS = 0b0001
   MOD_DIALIT = 0b0010
   ModVals = {
     CHAR_STRESS => MOD_STRESS,
     CHAR_DIALIT => MOD_DIALIT,
-  }
+  }.deep_freeze
 
   TransMap = {
     0 => Simple,
     MOD_STRESS => Stressed,
     MOD_DIALIT =>  Dialitics,
     MOD_STRESS | MOD_DIALIT => StressedDialitics,
-  }
+  }.deep_freeze
 
   # Outputs the mod-char if it's not meant
   # for modding anything, and returns the
