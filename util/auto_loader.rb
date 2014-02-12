80c135c4714eff07a7c0e14864fbf986f3cdebbc
Nikos Mouchtaris <mouchtaris@gmail.com>
2014-02-12 14:46:05 +0200

    Improve AutoLoader logging readability

--- util/auto_loader.rb
+++ util/auto_loader.rb
@@ -16,8 +16,12 @@ module AutoLoader
   def const_missing name
     full_name = "#{self.name}::#{name}"
     
-    $AutoLoading_nesting ||= 0
-    STDERR.print "- #{'    ' * $AutoLoading_nesting}Autoloading #{full_name} . . ."
+    @auto_loader_nesting ||= 0
+    STDERR.printf('[AL] %s%-*s',
+      '| ' * @auto_loader_nesting   ,
+      30 - @auto_loader_nesting * 2 ,
+      full_name                     ,
+      )
 
     @auto_loader_looked_for ||= {}
     if @auto_loader_looked_for.has_key? full_name
@@ -25,19 +29,18 @@ module AutoLoader
       result = @auto_loader_looked_for[full_name]
       if result.is_a? FalseClass
       then raise "#{full_name} already looked for -- nothing like it" 
-      else STDERR.puts " -- cached: #{result}"
+      else STDERR.puts "CACHE:#{result}"
       end
       result
     else
       full_path = AutoLoader.path_for_module_name(full_name)
-      STDERR.puts" -- from #{full_path} . . ."
-      $AutoLoading_nesting += 1
+      STDERR.puts "FILE:#{full_path}"
+      @auto_loader_nesting += 1
       raise "Could not load #{full_path} for #{full_name}" unless require full_path.to_s
-      $AutoLoading_nesting -= 1
+      @auto_loader_nesting -= 1
 
       raise "Could not find #{name} in #{self.name}" unless result = const_get(name)
-      
-      STDERR.puts "  #{'    ' * $AutoLoading_nesting}#{full_name} OK"
+
       @auto_loader_looked_for[full_name] = result || false
       result
     end
